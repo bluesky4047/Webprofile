915fe713f41c5625da1ccc394abc40e6882eb9e0
bluesky4047 <161824608+bluesky4047@users.noreply.github.com>
2025-10-21 15:56:16 +0700

    fix

--- FE-WebProfile/src/components/contact/Address.jsx
+++ FE-WebProfile/src/components/contact/Address.jsx
@@ -6,7 +6,7 @@ const Address = ({ item }) => {
 
   return (
     <div
-      className="max-w-84 p-3 md:p-3.75 lg:p-6 flex xs:not-odd:my-3 rounded-[10px] bg-white  hover:scale-[1] duration-450  cursor-pointer hover:shadow-[0px_0px_37px_5px_rgba(0,_0,_0,_0.1)] shadow-gray-200 max-sm:mx-auto"
+      className="max-w-95 p-3 md:p-3.75 lg:p-6 flex xs:not-odd:my-3 rounded-[10px] bg-white  hover:scale-[1] duration-450  cursor-pointer hover:shadow-[0px_0px_37px_5px_rgba(0,_0,_0,_0.1)] shadow-gray-200 max-sm:mx-auto"
       /* Scale effect:  */
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
